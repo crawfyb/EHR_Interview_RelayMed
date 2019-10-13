504116d8f528114f8c9ed3a8052dabe66bc44cd7
crawfyb <crawford.brown@outlook.com>
2019-10-13 17:31:59 +0100

    OrderType Models

--- EHR/src/main/java/com/EHR/EHR/Models/OrderType.java
+++ EHR/src/main/java/com/EHR/EHR/Models/OrderType.java
@@ -1,4 +1,41 @@
 package com.EHR.EHR.Models;
 
 public class OrderType {
-}
+    
+        private int orderCode;
+
+        private String orderName;
+
+        private String specimenType;
+
+        public OrderType(int orderCode, String orderName, String specimenType) {
+            this.orderCode = orderCode;
+            this.orderName = orderName;
+            this.specimenType = specimenType;
+        }
+
+        public int getOrderCode() {
+            return orderCode;
+        }
+
+        public void setOrderCode(int orderCode) {
+            this.orderCode = orderCode;
+        }
+
+        public String getOrderName() {
+            return orderName;
+        }
+
+        public void setOrderName(String orderName) {
+            this.orderName = orderName;
+        }
+
+        public String getSpecimenType() {
+            return specimenType;
+        }
+
+        public void setSpecimenType(String specimenType) {
+            this.specimenType = specimenType;
+        }
+
+    }
