82316d3ebd8f8ca7679a5844f9cbf1b45084d627
crawfyb <crawford.brown@outlook.com>
2019-10-15 12:08:44 +0100

    error for mike

--- EHR/src/main/java/com/EHR/EHR/Repositories/PatientRepositories/PatientRepository.java
+++ EHR/src/main/java/com/EHR/EHR/Repositories/PatientRepositories/PatientRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.EHR.EHR.Models.Patient;
 
+import java.util.List;
+
 public interface PatientRepository  extends JpaRepository<Patient, Long>, PatientRepositoryCustom {
+    List<Patient> findPatientByMRN(String mRN);
 }
--- EHR/src/test/java/com/EHR/EHR/EhrApplicationTests.java
+++ EHR/src/test/java/com/EHR/EHR/EhrApplicationTests.java
@@ -1,16 +1,40 @@
 package com.EHR.EHR;
 
+import com.EHR.EHR.Models.Patient;
+import com.EHR.EHR.Repositories.OrderTypeRepositories.OrderTypeRepository;
+import com.EHR.EHR.Repositories.PatientOrderReposiotories.PatientOrderRepository;
+import com.EHR.EHR.Repositories.PatientRepositories.PatientRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EhrApplicationTests {
 
+	@Autowired
+	PatientOrderRepository patientOrderRepository;
+
+	@Autowired
+	PatientRepository patientRepository;
+
+	@Autowired
+	OrderTypeRepository orderTypeRepository;
+
 	@Test
 	public void contextLoads() {
 	}
 
+
+//	@Test
+//	public void canGetPatientByMRN() {
+//		List<Patient> found = patientRepository.findPatientByMRN("OGH00123");
+//		assertEquals("12/03/1983", found.get(0).getDateOfBirth());
+//	}
 }
